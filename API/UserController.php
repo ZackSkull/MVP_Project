49a41de39b2ddab95fe8d057bbdcd43565f996f8
Achmad_Dzaki <131113832@students.mikroskil.ac.id>
2016-10-24 12:32:51 +0700

    Initiate
    API dan Android file

---
+++ API/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\User;
+
+use Hash;
+use DB;
+use Auth;
+
+class UserController extends Controller
+{
+    function doRegister (Request $request) {
+    	$_userexist = User::where('email', $request['email'])->get()->first();
+    	if ($_userexist == null) {
+	    	$_userdatas = array(
+	    		'name'     => $request['name'],
+	    		'email'    => $request['email'],
+	    		'password' => Hash::make($request['password'])
+	    	);
+
+	    	$_user = new User;
+	    	$_user::create($_userdatas);
+
+	    	return response()->json(['message'=> 'Register Success.', 'code'=> '201']);    		
+    	} else {
+    		return response()->json(['message'=> 'Sorry, email already exist.', 'code'=> '401']);
+    	}
+    }
+
+    function doLogin (Request $request) {
+    	$_userdatas = array(
+    		'email'    => $request['email'],
+    		'password' => $request['password']
+    	);
+
+    	if(Auth::attempt($_userdatas))
+    		return response()->json(['message'=> 'Login Success', 'code'=> '200']);
+    	return response()->json(['message'=> 'Email and password does not match.', 'code'=> '401']);
+
+    }
+}
