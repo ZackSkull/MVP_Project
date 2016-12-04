58c72eeb013411601d274139eb58ee64dc53c5ff
Achmad_Dzaki <131113832@students.mikroskil.ac.id>
2016-12-04 15:15:28 +0700

    Edit interface auth

---
+++ PhoTradeProject/app/src/main/java/com/mvp/photradeproject/Model/Responses/ThreadResponse.java
@@ -0,0 +1,19 @@
+package com.mvp.photradeproject.Model.Responses;
+
+import com.mvp.photradeproject.Model.Basic.Thread;
+
+/**
+ * Created by Zackzack on 04/12/2016.
+ */
+
+public class ThreadResponse {
+    private Thread thread;
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+}
