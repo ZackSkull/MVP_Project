4c507413c9415623cff8b16b0c3c90733f633ed7
BukhariMuslim <13ukh421@gmail.com>
2017-01-06 19:11:41 +0700

    Edit Profile(Bugs)

--- PTrade/app/src/main/java/com/mvp/ptrade/View/Fragment/ProfileFragment.java
+++ PTrade/app/src/main/java/com/mvp/ptrade/View/Fragment/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.mvp.ptrade.View.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,13 @@ import com.mvp.ptrade.Presenter.iPresenterUserResponse;
 import com.mvp.ptrade.R;
 import com.mvp.ptrade.View.Activity.MainActivity;
 import com.mvp.ptrade.View.Activity.ProfileActivity;
+
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Zackzack on 20/12/2016.
@@ -45,7 +53,8 @@ public class ProfileFragment extends Fragment implements iPresenterUserResponse 
         phone = (EditText) _view.findViewById(R.id.frag_pro_phone);
         bio = (EditText) _view.findViewById(R.id.frag_pro_bio);
         gender = (Spinner) _view.findViewById(R.id.frag_pro_spinner_gender);
-        date = (EditText) _view.findViewById(R.id.frag_pro_date);
+//        date = (EditText) _view.findViewById(R.id.frag_pro_date);
+        datePicker = (DatePicker) _view.findViewById(R.id.frag_pro_date);
         save = (Button) _view.findViewById(R.id.frag_pro_btnsave);
         close = (Button) _view.findViewById(R.id.frag_pro_btnclose);
 
@@ -79,26 +88,41 @@ public class ProfileFragment extends Fragment implements iPresenterUserResponse 
     }
     private void setsession(){
 //<<<<<<< HEAD
-        Log.d("Name:", sessionManager.getUserLoggedIn().getName());
-        Log.d("Username:", sessionManager.getUserLoggedIn().getUsername());
-        Log.d("Email:", sessionManager.getUserLoggedIn().getEmail());
-        Log.d("City:", sessionManager.getUserLoggedIn().getCity());
-        Log.d("Address:", sessionManager.getUserLoggedIn().getAddress());
-        Log.d("Phone:", sessionManager.getUserLoggedIn().getPhone());
-        Log.d("Bio:", sessionManager.getUserLoggedIn().getBio());
-        Log.d("Date:", sessionManager.getUserLoggedIn().getBorndate());
-
-        name.setText(sessionManager.getUserLoggedIn().getName());
+//        Log.d("Name:", String.valueOf(sessionManager.getUserLoggedIn().getName()));
+//        Log.d("Username:", String.valueOf(sessionManager.getUserLoggedIn().getUsername()));
+//        Log.d("Email:", String.valueOf(sessionManager.getUserLoggedIn().getEmail()));
+//        Log.d("City:", String.valueOf(sessionManager.getUserLoggedIn().getCity()));
+//        Log.d("Address:", String.valueOf(sessionManager.getUserLoggedIn().getAddress()));
+//        Log.d("Phone:", String.valueOf(sessionManager.getUserLoggedIn().getPhone()));
+//        Log.d("Bio:", String.valueOf(sessionManager.getUserLoggedIn().getBio()));
+//        Log.d("Date:", String.valueOf(sessionManager.getUserLoggedIn().getBorndate()));
+//
+        name.setText(String.valueOf(sessionManager.getUserLoggedIn().getName()));
         username.setText(sessionManager.getUserLoggedIn().getUsername());
-        email.setText(sessionManager.getUserLoggedIn().getEmail());
-        city.setText(sessionManager.getUserLoggedIn().getCity());
-        address.setText(sessionManager.getUserLoggedIn().getAddress());
-        phone.setText(sessionManager.getUserLoggedIn().getPhone());
-        bio.setText(sessionManager.getUserLoggedIn().getBio());
-        date.setText(sessionManager.getUserLoggedIn().getBorndate());
-        if (sessionManager.getUserLoggedIn().getGender() == 1)
+        email.setText(String.valueOf(sessionManager.getUserLoggedIn().getEmail()));
+        city.setText(String.valueOf(sessionManager.getUserLoggedIn().getCity()));
+        address.setText(String.valueOf(sessionManager.getUserLoggedIn().getAddress()));
+        phone.setText(String.valueOf(sessionManager.getUserLoggedIn().getPhone()));
+        bio.setText(String.valueOf(sessionManager.getUserLoggedIn().getBio()));
+//        date.setText(String.valueOf(sessionManager.getUserLoggedIn().getBorndate()));
+        SimpleDateFormat _df = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa");
+        Date _d = new Date();
+        Calendar _cal = new Calendar.getInstance();
+
+
+        try {
+            _d = _df.parse(String.valueOf(sessionManager.getUserLoggedIn().getBorndate()));
+            _cal.setTime(_d);
+        } catch (ParseException _e){
+            _e.printStackTrace();
+        }
+
+        datePicker.init(_cal.YEAR, _cal.MONTH, _cal.DATE, null);
+        gender.setSelection(sessionManager.getUserLoggedIn().getGender());
+//        if (sessionManager.getUserLoggedIn().getGender() == 1)
 //=======
-//        name.setText(sessionManager.getUserLoggedIn().getUsername());
+//        if (sessionManager.getUserLoggedIn().getName() != null)
+//            name.setText(sessionManager.getUserLoggedIn().getName());
 //        username.setText(sessionManager.getUserLoggedIn().getUsername());
 //        email.setText(sessionManager.getUserLoggedIn().getEmail());
 //        if (sessionManager.getUserLoggedIn().getCity() != null)
@@ -111,11 +135,11 @@ public class ProfileFragment extends Fragment implements iPresenterUserResponse 
 //            bio.setText(sessionManager.getUserLoggedIn().getBio());
 //        if (sessionManager.getUserLoggedIn().getBorndate() != null)
 //            date.setText(sessionManager.getUserLoggedIn().getBorndate());
-//        if ((sessionManager.getUserLoggedIn().getGender() == true))
+//        if (sessionManager.getUserLoggedIn().getGender() == 1)
 //>>>>>>> 748d713305932df87d0eab74862a5d428eb3e18f
-            gender.setSelection(1);
-        else
-            gender.setSelection(2);
+//            gender.setSelection(0);
+//        else
+//            gender.setSelection(1);
     }
 
     @Override
